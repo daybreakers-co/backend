737ab12ab540403f323ee3a24d41b02eaeea10f1
Robert Beekman <robert@matsimitsu.nl>
2017-10-28 10:23:55 +0200

    Use GraphQl tracing

--- app/graphql/day_breakers_schema.rb
+++ app/graphql/day_breakers_schema.rb
@@ -2,6 +2,7 @@ DayBreakersSchema = GraphQL::Schema.define do
   mutation(Types::MutationType)
   query(Types::QueryType)
   use GraphQL::Batch
+  use GraphQL::Tracing::AppsignalTracing
 
   resolve_type ->(type, obj, ctx) do
     "Types::#{obj.class}Type".constantize
