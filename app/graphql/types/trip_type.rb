5e3f9af51137cc1c317b1af3b69b1df84422e765
Robert Beekman <robert@matsimitsu.nl>
2017-10-28 10:36:13 +0200

    Add argument to Trip photos field, that samples returned photos

--- app/graphql/types/trip_type.rb
+++ app/graphql/types/trip_type.rb
@@ -7,8 +7,12 @@ Types::TripType = GraphQL::ObjectType.define do
   field :subtitle, types.String
 
   field :photos, types[Types::PhotoType] do
+    argument :sample, types.Int
     resolve ->(obj, args, context) do
-      ForeignKeyLoader.for(Photo, :photographic_id).load(obj.photographic_ids)
+      ids = obj.photographic_ids
+      ids = ids.sample(args[:sample]) if args[:sample] && args[:sample] > 0
+
+      ForeignKeyLoader.for(Photo, :photographic_id).load(ids)
     end
   end
 
