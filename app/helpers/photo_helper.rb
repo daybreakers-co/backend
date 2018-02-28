d1332937b913682c084807c73c0aa8041d2ca079
Robert Beekman <robert@matsimitsu.nl>
2018-02-28 20:54:54 +0100

    Fix development photo url

--- app/helpers/photo_helper.rb
+++ app/helpers/photo_helper.rb
@@ -1,9 +1,9 @@
 module PhotoHelper
   def photo_cdn_url(obj, options={})
     if ActionController::Base.asset_host
-      [ActionController::Base.asset_host, Rails.env, obj.image_uid].join('/')
+      [ActionController::Base.asset_host, Rails.env, obj.image_uid].join("/")
     else
-      photo_url(obj.id, :format => File.extname(obj.image_uid))
+      photo_url(obj.id, :format => File.extname(obj.image_uid).delete("."))
     end
   end
 end
